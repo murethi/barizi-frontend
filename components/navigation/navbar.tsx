1c419a7167546975b755ac4b0e82d0c8d58271c5
murethi <murethibrian@outlook.com>
2024-03-31 02:00:09 +0300

    design: navigation bar

---
+++ components/navigation/navbar.tsx
@@ -0,0 +1,34 @@
+import {auth} from "@/auth";
+import Image from "next/image";
+import {UserIcon} from "@/components/navigation/user-icon";
+import Link from "next/link";
+import {FiUser} from "react-icons/fi";
+import {ClipboardIcon} from "@radix-ui/react-icons";
+
+export const Navbar =async ()=>{
+    const session = await auth();
+    if(!session){
+        return ;
+    }
+    return (
+        <nav className={"flex items-center justify-between px-2 shadow-md py-1"}>
+            <div>
+                <a href="/products">
+                    <Image width={120} height={80} src={'/logo-clear.png'} alt={"Barizi Communications"}/>
+                </a>
+            </div>
+            <ul className={"text-sm text-zinc-600 flex space-x-3 "}>
+                <li>
+                    <Link className={"hover:underline hover:text-blue-600 flex items-center space-x-1"} href={"/profile"}><ClipboardIcon className={"w-4 -h-4"}/> <span>Products</span></Link>
+
+                </li>
+                <li>
+                    <Link className={"hover:underline hover:text-blue-600 flex items-center space-x-1"} href={"/profile"}><FiUser className={"w-4 -h-4"}/> <span>Profile</span></Link>
+                </li>
+            </ul>
+            <div>
+                <UserIcon/>
+            </div>
+        </nav>
+    );
+}
