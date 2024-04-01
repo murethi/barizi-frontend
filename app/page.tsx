66e8604645a15b8829b1b53a950435bf01ce73d6
murethi <murethibrian@outlook.com>
2024-04-01 04:33:00 +0300

    chore: remove redirect

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,6 @@ import {redirect} from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
-  if(session){
-      redirect(`/products`);
-  }
 
 
   return (
