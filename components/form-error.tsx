1705647d70a957239cf2347bba1cfb9f171ac365
murethi <murethibrian@outlook.com>
2024-04-01 04:30:07 +0300

    design: form error component

---
+++ components/form-error.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {PiWarningCircle} from "react-icons/pi";
+
+
+interface FormErrorProps{
+    message?:string;
+}
+export const FormError = ({message}:FormErrorProps) => {
+    if(!message){
+        return null;
+    }
+    return (
+        <div className={"bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive"}>
+            <PiWarningCircle className={"w-4 h-4"}/>
+            <p>{message}</p>
+        </div>
+    );
+};
+
