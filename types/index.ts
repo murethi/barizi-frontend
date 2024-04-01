3cf221c6cbe022d4ff7b95566feeaca218dac29e
murethi <murethibrian@outlook.com>
2024-04-01 04:24:49 +0300

    chore: add id prop to product. create update product interface

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,11 @@
 export interface Product{
+    id:string;
     name:string;
     description:string;
     minimumStockLevel:number;
     currentStock:number;
+}
+
+export interface UpdateProductProps{
+    id:string;
 }
